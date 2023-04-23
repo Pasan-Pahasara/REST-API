c9626baeb730f24bc2d0d4f39b9c897500547b9d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-23 13:50:34 +0530

    Added Item Model

---
+++ src/models/Item.ts
@@ -0,0 +1,37 @@
+import { Document, Schema, model } from "mongoose";
+
+interface IItem extends Document {
+  itemCode: string;
+  name: string;
+  description: string;
+  qty: number;
+  unitPrice: number;
+}
+
+const ItemSchema = new Schema(
+  {
+    itemCode: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    qty: {
+      type: Number,
+      required: true,
+    },
+    unitPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const user = model("Item", ItemSchema);
