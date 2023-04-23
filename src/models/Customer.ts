abbec94f683f4f5c6070dbda9bdaa53e2e7a0a6a
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-23 13:22:01 +0530

    Added Customer Model

---
+++ src/models/Customer.ts
@@ -0,0 +1,27 @@
+import { Document, Schema, model } from "mongoose";
+
+interface ICustomer extends Document {
+  nic: string;
+  name: string;
+  address: string;
+}
+
+const CustomerSchema = new Schema(
+  {
+    nic: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const user = model("Customer", CustomerSchema);
