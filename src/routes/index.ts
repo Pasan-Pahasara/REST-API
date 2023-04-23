974dc89b012dd589312228ad98be792d1b376f54
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-23 14:01:17 +0530

    Set ItemRoutes in the index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import PostRoutes from "./PostRoutes";
 import UserRoutes from "./UserRoutes";
 import CustomerRoutes from "./CustomerRoutes";
+import ItemRoutes from "./ItemRoutes";
 
 const router: Router = Router();
 const url_prefix = "api/v1";
@@ -9,5 +10,6 @@ const url_prefix = "api/v1";
 router.use(`${url_prefix}/user`, new UserRoutes().getRouter);
 router.use(`${url_prefix}/post`, new PostRoutes().getRouter);
 router.use(`${url_prefix}/customer`, new CustomerRoutes().getRouter);
+router.use(`${url_prefix}/item`, new ItemRoutes().getRouter);
 
 export default router;
