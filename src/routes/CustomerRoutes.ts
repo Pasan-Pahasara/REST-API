3b7ba062a6ca24641c006dfad65efb0e800a3945
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-23 13:38:28 +0530

    Added CustomerRoutes.ts

---
+++ src/routes/CustomerRoutes.ts
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import CustomerController from "../controllers/CustomerController";
+
+export default class CustomerRoutes {
+  private router: Router = express.Router();
+  private customerController: CustomerController = new CustomerController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    //POST /api/v1/post
+    this.router.post("/", this.customerController.addCustomer);
+
+    //GET /api/v1/post
+    this.router.get("/", this.customerController.retrieveAllCustomers);
+    
+    //PUT /api/v1/post/:id
+    this.router.put("/:nic", this.customerController.updateCustomer);
+    
+    //DELETE /api/v1/post/:id
+    this.router.delete("/:nic", this.customerController.deleteCustomer);
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
